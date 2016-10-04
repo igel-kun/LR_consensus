77bc67b5370b989fc863c0f0ffd5dc9f5a24832c
scornava <celine.scornavacca@univ-montp2.fr>
2016-10-04 12:31:32 +0200

    triplet stuff

--- src/NodeInfos.h
+++ src/NodeInfos.h
@@ -25,18 +25,7 @@ class NodeInfos  {
 		int numberOfDescendents;
 		int stId;
 		int preorder;
-
-
-		//not used for now
-
-	    vector<int> clade;
-		vector<string> cladeNames;
-		vector <MyNode*>  cladeNode;
-		vector <MyNode*>  cladeNodeAbove;
-		vector <MyNode*> cladeNodeAboveDuplications;
-		int counter;
-		bool square;	
-		bool visited;	
+		pair<int, int> clade;
 
 	public:
 
@@ -81,124 +70,14 @@ class NodeInfos  {
 		int getNumberOfDescendents() const{
 			return numberOfDescendents;
 		}
-	
-		//not used for now		
-
-		void copyInClade(vector <int> leaves){
-			(clade).clear(); 
-			for( unsigned int i = 0; i <  leaves.size(); i++ ){
-     				(clade).push_back( leaves[i] );
-			}
+		
+		void setClade(pair <int, int> inds){
+			clade=inds;
 		}
 		
-		void copyInCladeNames(vector <string> leaves){
-			(cladeNames).clear(); 
-			for( unsigned int i = 0; i <  leaves.size(); i++ ){
-     				(cladeNames).push_back( leaves[i] );
-			}
-		}
-		
-		
-		void copyInCladeNode(vector <MyNode *> leaves){
-			(cladeNode).clear();
-			for( unsigned int i = 0; i <  leaves.size(); i++ ){
-     				(cladeNode).push_back(  leaves[i] );
-			}
-		}
-		
-		void copyInCladeNodeAbove(vector <MyNode *> leaves){
-			(cladeNodeAbove).clear();
-			for( unsigned int i = 0; i <  leaves.size(); i++ ){
-     				(cladeNodeAbove).push_back(  leaves[i] );
-			}
-		}	
-		
-		void copyInCladeNodeAboveDuplications(vector <MyNode *> leaves){
-			(cladeNodeAboveDuplications).clear();
-			for( unsigned int i = 0; i <  leaves.size(); i++ ){
-     				(cladeNodeAboveDuplications).push_back(  leaves[i] );
-			}
-		}	
-		
-		void copyInCladeNodeAboveNoClearDuplications(vector <MyNode *> leaves){
-			for( unsigned int i = 0; i <  leaves.size(); i++ )
-     			(cladeNodeAboveDuplications).push_back(  leaves[i] );
-		}	
-			
-		void copyInCladeNodeAboveNoClear(vector <MyNode *> leaves){
-			for( unsigned int i = 0; i <  leaves.size(); i++ )
-     			(cladeNodeAbove).push_back(  leaves[i] );
-		}	
-		
-		void setCladeNodeAbove(vector <MyNode *> leaves){
-			cladeNodeAbove = leaves;
-		}
-		
-		void setCladeNodeAboveDuplications(vector <MyNode *> leaves){
-			cladeNodeAboveDuplications = leaves;
-		}
-		
-		vector <MyNode * >getCladeNode(){
-			return cladeNode;
-		} 
-		vector <MyNode * >getCladeNodeAbove(){
-			return cladeNodeAbove;
-		} 
-		
-		vector <MyNode * >getCladeNodeAboveDuplications(){
-			return cladeNodeAboveDuplications;
-		} 
-		
-		vector<int> getClade() const{
+		pair <int, int> getClade(){
 			return clade;
-		};
-		
-		vector<string> getCladeNames() const{
-			return cladeNames;
-		};
-	
-		
-		void eraseCladeNode(){
-			int j= cladeNode.size();
-			for(int i = 0; i <  j; i++ ){
-     				(cladeNode).pop_back();
-			}
 		}
-		
-		void eraseCladeNodeAbove(){	
-			int j = cladeNodeAbove.size();
-			for(int i = 0; i <  j; i++ ){
-     				(cladeNodeAbove).pop_back();
-			}
-		}
-		
-		void eraseCladeNodeAboveDuplications(){	
-			int j = cladeNodeAboveDuplications.size();
-			for(int i = 0; i <  j; i++ ){
-     				(cladeNodeAboveDuplications).pop_back();
-			}
-		}
-					
-		void setVisited(bool a) {visited = a;};
-		bool getVisited() const{return visited;};
-		
-		void setSquare(bool a) { square = a;};
-		bool getSquare() const{return square;};
-		
-		void setCounter(int a) {counter= a;};
-		void addCounter() {counter ++;};
-		int getCounter() const{return counter;};	
-		
-		void copyInCladeNodeLinear(MyNode *leaves){
-			//(cladeNode).clear();
-			(cladeNode).push_back(  leaves );
-		}
-		
-		void copyInCladeNodeAboveLinear(MyNode *leaves){
-			//(cladeNodeAbove).clear();
-			(cladeNodeAbove).push_back(  leaves );		
-		}	
-
 		
 		
 };
